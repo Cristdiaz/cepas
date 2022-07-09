461cf2d1a9c11f858bcde490859f112063bedd60
Cristina Diaz <hagaren_022@hotmail.com>
2022-07-09 12:11:46 -0400

    cepas1

---
+++ app/models/vino.rb
@@ -0,0 +1,5 @@
+class Vino < ApplicationRecord
+    has_many :ensamblajes
+    has_many :cepas, through: :ensamblajes
+    accepts_nested_attributes_for :ensamblajes
+end
---
+++ db/seeds.rb
@@ -0,0 +1,38 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+#CEPAS
+Cepa.create(nombre: 'Cabernet Sauvignon')
+Cepa.create(nombre: 'Cabernet')
+Cepa.create(nombre: 'Syrah')
+Cepa.create(nombre: 'Tannat')
+Cepa.create(nombre: 'Malbec')
+Cepa.create(nombre: 'Bonarda')
+Cepa.create(nombre: 'Petit Verdot')
+Cepa.create(nombre: 'Merlot')
+Cepa.create(nombre: 'Carmenere')
+Cepa.create(nombre: 'Sangiovese')
+Cepa.create(nombre: 'Tempranillo')
+Cepa.create(nombre: 'Marselan')
+Cepa.create(nombre: 'Pinot Noir')
+Cepa.create(nombre: 'Carignan')
+Cepa.create(nombre: 'Cinsaunt')
+
+#VINOS:
+Vino.create(nombre: 'Viñedo Chadwick')
+Vino.create(nombre: 'Concha y Toro')
+Vino.create(nombre: 'Amayna')
+Vino.create(nombre: 'Ventisquero Herú')
+Vino.create(nombre: 'Misiones de Rengo')
+Vino.create(nombre: 'Casillero del Diablo')
+Vino.create(nombre: 'Las Mulas')
+Vino.create(nombre: 'Viña Falernia')
+Vino.create(nombre: 'Santa Ema')
+Vino.create(nombre: 'Wynemakers')
+Vino.create(nombre: 'Almaviva')
+Vino.create(nombre: 'Isla Negra')
